d35972e41fc1c11d82f30ca7c2efd3a6af78994c
Anneke Hugenberg <Annekehugenberg@proton.me>
2024-05-08 21:54:02 +0200

    add search functionality to DOM element and log results to console using the publisher-subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,18 +1,13 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-///////////////////////////////////////
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -21,15 +16,32 @@ const controlRecipes = async function () {
     await model.loadRecipe(id);
 
     // h2 2) rendering recipe
-    recipeView.render(model.state.recipe); // data from ln above is put into the render-method in recipeView.js
+    recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
   }
 };
-controlRecipes();
+
+const controlSearchResults = async function () {
+  try {
+    // 1) get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) render results
+    console.log(model.state.serach.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
 
 const init = function () {
   // subscriber
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,15 +1,20 @@
 import { async } from 'regenerator-runtime';
 import { API_URL } from './config.js';
 import { getJSON } from './helpers.js';
+// import { keyFor } from 'core-js/fn/symbol';
 
 // h1 state-object
 export const state = {
   recipe: {},
+  serach: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -26,6 +31,27 @@ export const loadRecipe = async function (id) {
   } catch (err) {
     // temp error handler
     console.error(`${err} 💣💣`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.serach.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.serach.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    // const data = await getJSON(`https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza`)
+  } catch (err) {
+    console.error(`${err} 💣💣💣`);
     throw err;
   }
 };
