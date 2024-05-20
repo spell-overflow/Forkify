ce445a8d63712d167e82586acc03c83a4e1d5436
Anneke Hugenberg <annekehugenberg@proton.me>
2024-05-20 19:27:55 +0200

    add functionality to add and delete bookmarks

--- src/js/model.js
+++ src/js/model.js
@@ -30,6 +30,9 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
   } catch (err) {
     // temp error handler
     console.error(`${err} 💣💣`);
@@ -79,4 +82,12 @@ export const addBookmark = function (recipe) {
 
   // Mark current recipe as bookmark
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+};
+
+export const deleteBookmark = function (id) {
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as not bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
 };
