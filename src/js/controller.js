327571f30e2eca5574ccf28f941691b12ecba57c
Anneke Hugenberg <Annekehugenberg@proton.me>
2024-05-22 21:55:31 +0200

    add functionality forshow add new recipe window and create an object from the given fields

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -88,6 +89,13 @@ const controlAddBookmark = function () {
 const controlBookbarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
+
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  // upload new recipe data
+};
+
 const init = function () {
   // subscriber
   bookmarksView.addHandlerRender(controlBookbarks);
@@ -96,6 +104,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
 
