f0c2caacf2b7e4f73f852858b59f53c1f51b6ba6
Anneke Hugenberg <Annekehugenberg@proton.me>
2024-05-27 20:11:13 +0200

    add welcom msg

--- src/js/controller.js
+++ src/js/controller.js
@@ -132,5 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('welcome to my application');
 };
 init();
